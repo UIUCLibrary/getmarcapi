c779d0f2c4aadb362f54a3ee7e73fb681fe34761
henry <henryborchers@yahoo.com>
2020-07-28 11:23:30 -0500

    add config

--- getmarcapi/app.py
+++ getmarcapi/app.py
@@ -1,5 +1,6 @@
 import os
-
+from typing import Mapping
+from .config import get_config
 from flask import Flask, Response, request
 from uiucprescon import getmarc2
 app = Flask(__name__)
@@ -15,13 +16,16 @@ def get_record() -> Response:
     bibid = request.args.get("bibid")
     if bibid is None:
         return Response("Missing required param bibid", status=422)
-    domain = os.environ.get('domain')
+
+    get_config(app)
+    domain = app.config.get('domain')
     if domain is None:
         return Response("Missing domain", status=500)
 
-    api_key = os.environ.get('api_key')
+    api_key = app.config.get('api_key')
     if api_key is None:
         return Response("Missing api key", status=500)
+
     try:
         server = getmarc2.records.RecordServer(domain, api_key)
         data = server.bibid_record(bibid)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,12 @@
 import pytest
 import getmarcapi
-
+from uiucprescon.getmarc2.records import RecordServer
 
 @pytest.fixture
 def client():
     getmarcapi.app.config['testing'] = True
+    getmarcapi.app.config['domain'] = "testing"
+    getmarcapi.app.config['api_key'] = "NA"
     with getmarcapi.app.test_client() as client:
         yield client
 
@@ -14,7 +16,10 @@ def test_root(client):
     assert rc.status_code == 200
 
 
-def test_get_record_xml(client):
+def test_get_record_xml(monkeypatch, client):
+    def mock_response(self, bibid, *args, **kwargs):
+        return ""
+    monkeypatch.setattr(RecordServer, "bibid_record", mock_response)
     rc = client.get('/record?bibid=12345')
     assert rc.status_code == 200
     assert rc.content_type == 'text/xml'
