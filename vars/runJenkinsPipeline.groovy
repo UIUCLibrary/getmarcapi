310158bf627dcc5f26fc136978241723ff0bd5d3
hborcher <hborcher@illinois.edu>
2026-05-19 13:42:56 -0500

    ci: use uv audit to audit lockfile instead of uv-secure

--- vars/runJenkinsPipeline.groovy
+++ vars/runJenkinsPipeline.groovy
@@ -309,11 +309,8 @@ def call(){
                                                     timeout(5)
                                                 }
                                                 steps{
-                                                    catchError(buildResult: 'UNSTABLE', message: 'uv-secure found issues', stageResult: 'UNSTABLE') {
-                                                        sh '''unset UV_INDEX_URL
-                                                              unset UV_EXTRA_INDEX_URL
-                                                              uv run --only-group=audit-dependencies --isolated uv-secure --disable-cache uv.lock
-                                                           '''
+                                                    catchError(buildResult: 'UNSTABLE', message: 'uv audit found issues', stageResult: 'UNSTABLE') {
+                                                        sh 'uv audit'
                                                     }
                                                 }
                                             }
